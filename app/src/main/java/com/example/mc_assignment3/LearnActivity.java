42e0e2c9ccd2aeca953f95046ed164cb33842d01
umairnaveed-se <umair_chishti98@hotmail.com>
2021-11-09 04:38:14 +0500

    added images and changes to learn activity

--- app/src/main/java/com/example/mc_assignment3/LearnActivity.java
+++ app/src/main/java/com/example/mc_assignment3/LearnActivity.java
@@ -6,12 +6,15 @@ import androidx.lifecycle.OnLifecycleEvent;
 
 import java.util.Arrays;
 import java.util.List;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -22,6 +25,7 @@ public class LearnActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
         List<Character> chars = Arrays.asList('أ', 'ب', 'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ک', 'ل', 'م', 'ن', 'و', 'ہ', 'ی');
+        List<Integer> indexType = Arrays.asList(1, 9, 5, 6, 3, 1, 1, 5, 6, 4, 7, 7, 3, 7, 3, 5, 6, 1, 1, 9, 2, 2, 4, 9, 8, 9, 1, 3);
         androidx.gridlayout.widget.GridLayout btncontainer = findViewById(R.id.btnCont);
         for (int i = 0; i < chars.size(); i++) {
             Button btn = new Button(this);
@@ -35,8 +39,8 @@ public class LearnActivity extends AppCompatActivity {
             btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    TextView field = findViewById(R.id.textView5);
-                    field.setText(btn.getText());
+                    //TextView field = findViewById(R.id.textView5);
+                    //field.setText(btn.getText());
                     for(int i =0; i < btncontainer.getChildCount(); i++){
                         View btntemp = btncontainer.getChildAt(i);
                         if(v instanceof Button){
@@ -44,6 +48,11 @@ public class LearnActivity extends AppCompatActivity {
                         }
                     }
                     btn.setBackgroundColor(getResources().getColor(R.color.custom2));
+                    ImageView imgview = findViewById(R.id.imageView2);
+                    Context context = imgview.getContext();
+                    String imgname = "alpha" + Integer.toString(indexType.get(chars.indexOf(btn.getText().toString().charAt(0))));
+                    int id = context.getResources().getIdentifier(imgname, "drawable", context.getPackageName());
+                    imgview.setImageResource(id);
                 }
             });
             btncontainer.addView(btn);
