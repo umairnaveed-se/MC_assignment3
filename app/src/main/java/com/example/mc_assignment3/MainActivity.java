aa3454693cde9f0c9b10c08e2a24f76f2a7122ce
umairnaveed-se <umair_chishti98@hotmail.com>
2021-11-08 23:13:10 +0500

    change in main activity

--- app/src/main/java/com/example/mc_assignment3/MainActivity.java
+++ app/src/main/java/com/example/mc_assignment3/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.mc_assignment3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
 
 import android.os.Bundle;
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openRepo(View view){
+        Uri uri = Uri.parse("https://github.com/umairnaveed-se/MC_assignment3/tree/master"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/mc_assignment3/homeActivity.java
@@ -0,0 +1,14 @@
+package com.example.mc_assignment3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
